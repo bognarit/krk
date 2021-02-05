791db2c881567d8cfdecb6b4be2d766029384562
akos.bognar <akos.bognar@agiles.de>
2021-02-05 22:30:41 +0100

    Homepage 14

---
+++ js/script.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+  // init is the start function that starts when page loads
+  init();
+};
+
+function init() {
+  addEventlistenerLinks();
+}
+
+function addEventlistenerLinks() {
+  // add eventlistener
+}
